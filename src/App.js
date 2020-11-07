675c33debcc7bf913acf1b97d69a97045c2d2bf5
Jackie Tian <jackie.tian@siteminder.com>
2020-11-08 09:56:36 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Link, NavLink, Route, Switch } from 'react-router-dom'
+import './App.css'
+import Login from './components/Login';
+import home from './components/Home'
+import Courses from './components/Courses'
+import Logout from './components/Logout';
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={Login} />
+      <ProtectedRoute path="/home" component={home} />
+      <ProtectedRoute path="/courses" component={Courses} />
+      <ProtectedRoute path="/logout" component={Logout} />
+    </Router>
   );
 }
 
